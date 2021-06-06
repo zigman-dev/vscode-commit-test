b578d23a93b83584aecedc944f691cbd2c1c88e1
JY Hsu <jy.hsu@realtek.com>
2021-06-06 23:59:29 +0800

    Remove commit-test functionalities for svn-based modem code-line

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,11 @@
 import * as vscode from 'vscode';
 
 import verifyEnvironment from "./verify";
-import { commitTest, commitTestChangelist, getTicketChangelist } from "./modem";
 import { preCommitTest } from "./cooper"
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand(
         'commit-test.verifyEnvironment', verifyEnvironment));
-    context.subscriptions.push(vscode.commands.registerCommand(
-        'commit-test.commitTest', commitTest));
-    context.subscriptions.push(vscode.commands.registerCommand(
-        'commit-test.commitTestChangelist', commitTestChangelist));
-    context.subscriptions.push(vscode.commands.registerCommand(
-        'commit-test.getTicketChangelist', getTicketChangelist));
     context.subscriptions.push(vscode.commands.registerCommand(
         'commit-test.preCommitTest', preCommitTest));
 }
